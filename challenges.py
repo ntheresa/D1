d58d205c5a4495c24c6c6528b27a3cf0c7356af9
ntheresa <ntheresa@umich.edu>
2018-09-14 14:30:08 -0400

    pushing files

--- challenges.py
+++ challenges.py
@@ -5,11 +5,11 @@ import json
 app = Flask(__name__)
 
 
-#Task 2 : Dynamic URLS 
+#Task 2 : Dynamic URLS
     #edit the view function to display 'Welcome to <course_name>' on localhost:5000/course/<course>
 @app.route('/course/<course_name>')
-def courseView():
-    return ""
+def courseView(course_name):
+    return "Welcome to " + course_name
 
 #Task 3.1 Basic HTML Form
     #Set the method and action of the HTML form, such that form data is sent to /result using POST method
@@ -19,7 +19,10 @@ def formView():
     html_form = '''
     <html>
     <body>
-    <form>
+    <form action = "http://localhost:5000/result" method = "POST">
+        <label> Enter an ingredient:
+        <input type="text" name="ingredient"/> </label>
+        <input type="submit" name="Submit"/>
     </form>
     </body>
     </html>
@@ -27,15 +30,18 @@ def formView():
     return html_form
 
 #Task 3.2 : Processing Form Data
-@app.route('/result', methods = ['GET', 'POST'])
+@app.route('/result', methods = ['GET','POST'])
 def resultView():
-    # Make an API request to Recipe API for the ingredient entered in the form and display the recipe results 
-    #Step 1 : Receive the ingredient from the form if request type is POST
-    #Step 2 : Create paramaters JSON with the ingredient received in step 1 in the form required by http://www.recipepuppy.com/about/api/
-    #Step 3 : Make an API request to Recipe API and parameters in Step 2 
-    #Step 4 : Parse the response from API request in JSON 
-    #Step 5 : Display the response in browser (remember : HTML takes only strings)
-    return ""
+    if request.method == 'POST':
+        ingredient = request.form.get("ingredient")
+        params = {}
+        params["i"] = ingredient
+        response = requests.get("http://www.recipepuppy.com/api/", params = params)
+        json_response = json.loads(response.text)
+        response_str = str(json_response)
+    return response_str
+
+
 
 
 if __name__ == '__main__':
